4c06e8512133ddab18c5cb31a1338fabd4254fed
artemiy <->
2020-04-01 21:27:03 +0300

    Initial Commit

---
+++ VKFriendsApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//	ViewController.swift
+// 	VKFriendsApp
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
